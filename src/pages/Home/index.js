9bb2dbebc0abda92dd8079ccc353474e76829e60
Jonathan Albuquerque <jonathan.albuquerque.dev@gmail.com>
2023-09-13 17:31:51 -0300

    criado a pagina home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,7 @@
 import { useState, useEffect } from "react";
 import api from "../../services/api";
+import { Link } from "react-router-dom";
+import "./style.css";
 
 //https://api.themoviedb.org/3/discover/movie?api_key=85ce3425b5f20922bf81d5bb096cb42a&language=pt-br
 
@@ -16,15 +18,28 @@ export default function Home() {
         },
       });
 
-      console.log(response);
+      setFilmes(response.data.results.slice(0, 10));
     }
 
     loadFilmes();
   }, []);
 
   return (
-    <div>
-      <h1>Home</h1>
+    <div className="container-main">
+      <div className="lista-filmes">
+        {filmes.map((filme) => {
+          return (
+            <article key={filme.id}>
+              <h2>{filme.title}</h2>
+              <img
+                src={`http://image.tmdb.org/t/p/original/${filme.poster_path}`}
+                alt={filme.title}
+              />
+              <Link to={`/filme/${filme.id}`}>Detalhes</Link>
+            </article>
+          );
+        })}
+      </div>
     </div>
   );
 }
